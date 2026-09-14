62be29777d7725f05e7336d11aa08e8b64712d20
Redonline <dev@redonline.me>
2026-09-14 16:15:49 -0700

    refactor(dendritic) migrate configuration.nix to dendritic pattern
    Migrate configration.nix from /etc/nixos to
    ~/nixos/modules/hosts/site1-ws-nix-01/configuration.nix
    
    Convert configuration.nix to flake-parts module

---
+++ modules/hosts/site1-ws-nix-01/configuration.nix
@@ -0,0 +1,112 @@
+{ self, inputs, ... }: {
+  flake.nixosModules."site1-ws-nix-01" = { pkgs, ... }: {
+    # Boot
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+    boot.kernelPackages = pkgs.linuxPackages_latest;
+
+    # Hibernation
+    boot.initrd.systemd.enable = true;
+    boot.resumeDevice = "/dev/mapper/cryptroot";
+    boot.kernelParams = ["mem_sleep_default=deep"];
+
+    swapDevices = [{
+      device = "/var/lib/swapfile";
+      size = 34 * 1024;
+    }];
+
+    services.logind.settings.Login = {
+      LidSwitch = "suspend-then-hibernate";
+      LidSwitchDocked = "ignore";
+      PowerKey = "hibernate";
+      PowerKeyLongPress = "poweroff";
+    };
+    
+    # Nix
+    nixpkgs.config.allowUnfree = true;
+    nix = {
+      settings.experimental-features = [ "flakes" "nix-command" ];
+      gc = {
+        automatic = true;
+        dates = "Sun 00:00:00";
+        persistent = true;
+      };
+
+      optimise = {
+        automatic = true;
+        dates = "Sun 01:00:00";
+        persistent = true;
+      };
+    };
+    
+    # Networking
+    networking = {
+      hostName = "site1-ws-nix-01";
+      domain = "redonline.lan";
+      networkmanager.enable = true;
+    };
+
+    # Power Management
+    services = {
+      tlp.enable = false;
+      power-profiles-daemon.enable = false;
+      auto-cpufreq.enable = true;
+    };
+
+    # Firmware
+    hardware.enableAllFirmware = true;
+    services.fwupd.enable = true;
+
+    # Locale
+    time.timeZone = "America/Phoenix";
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    # Packages
+    environment.systemPackages = with pkgs; [
+      neovim
+      wget
+      curl
+      zsh
+      nvme-cli
+      smartmontools
+      pciutils
+      usbutils
+      dnsutils
+      nmap
+      ripgrep
+      fd
+      fzf
+      btop
+      tre-command
+      gh
+      bitwarden-cli
+    ];
+
+    programs.git = {
+      enable = true;
+      config = {
+        init.defaultBranch = "main";
+        core.editor = "nvim -f";
+        user.name = "Redonline";
+        user.email = "dev@redonline.me";
+      };
+    };
+    programs.zsh.enable = true;
+
+    # Users
+    users.users."joshua" = {
+      description = "Joshua Myers";
+      name = "joshua";
+      createHome = true;
+      isNormalUser = true;
+      extraGroups = [ "wheel" "networkmanager" ];
+      initialPassword = "changeme";
+      home = "/home/joshua";
+      shell = pkgs.zsh;
+    };
+
+    # State Version - DO NOT CHANGE THIS EVER!!!!!!!!1!1
+    system.stateVersion = "26.05";
+  };
+}
+
