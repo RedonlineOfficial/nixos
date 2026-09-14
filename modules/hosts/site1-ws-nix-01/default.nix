de6f7e99e282ea374cfe7bc9fa1d30f3ad0d8b5e
Redonline <dev@redonline.me>
2026-09-14 16:20:25 -0700

    refactor(dendritic): create nixosConfigurations module
    Defines a flake-parts nixosConfigurations module that imports
    inputs.disko.nixosModules.disko (will migrate this out of this module
    later), nixosModules.disko_2disk-luks-split-home (the current disko
    flake module that defines the disk layout),
    nixosModules."site1-ws-nix-01" the module that defines all of our host
    options (currently just a converted configuration.nix from install), and
    ./_hardware-configuration ('_' tells import-tree to ignore this file and
    can be imported without converting into a flake-parts module)

---
+++ modules/hosts/site1-ws-nix-01/default.nix
@@ -0,0 +1,11 @@
+{ self, inputs, ... }: {
+  flake.nixosConfigurations."site1-ws-nix-01" = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = [
+      inputs.disko.nixosModules.disko
+      self.nixosModules.disko_efi-2disk-luks-split-home
+      self.nixosModules."site1-ws-nix-01"
+      ./_hardware-configuration.nix
+    ];
+  };
+}
