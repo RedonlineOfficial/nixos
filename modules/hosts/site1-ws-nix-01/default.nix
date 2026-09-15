384c46ba35679c32f1383d805faaf65607b17ce2
Redonline <dev@redonline.me>
2026-09-15 07:08:10 -0700

    fix: add user module to host entrypoint
    Adds the user-joshua module to the site1-ws-nix-01 module imports.
    Failing to add this resulted in the user not existing anymore.  Required
    a roll back to a previous generation to fix.

--- modules/hosts/site1-ws-nix-01/default.nix
+++ modules/hosts/site1-ws-nix-01/default.nix
@@ -6,6 +6,7 @@
       self.nixosModules.disko_efi-2disk-luks-split-home
       self.nixosModules."site1-ws-nix-01"
       ./_hardware-configuration.nix
+      self.nixosModules."user-joshua"
     ];
   };
 }
